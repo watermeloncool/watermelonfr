a4448a42f69fea538c634949287d5fd5fee7831b
watermeloncool <149166592+watermeloncool@users.noreply.github.com>
2024-02-18 16:56:40 +0100

    Delete code directory

--- code/redirect.js
+++
@@ -1,5 +0,0 @@
-const lang = navigator.language.toLowerCase();
-if (lang.startsWith('fr')) {}
-else if (lang.startsWith('ru')) {window.location.href = '/ru';}
-else if (lang.startsWith('de')) {window.location.href = '/de';}
-else {window.location.href = '/en';}
