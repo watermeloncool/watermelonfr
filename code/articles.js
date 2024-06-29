cc91053e3b0bf241be9e46558205a221d0460599
watermeloncool <149166592+watermeloncool@users.noreply.github.com>
2024-06-29 17:38:59 +0200

    Add files via upload

--- code/articles.js
+++ code/articles.js
@@ -1,6 +1,6 @@
-document.head.innerHTML(`
+document.head.insertAdjacentHTML('beforeend', `
     <meta charset="utf-8">
-    <title>Watermelon Articles</title>
+    <title>Watermelon</title>
     <meta name="robots" content="index,follow">
     <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0">
 `);
