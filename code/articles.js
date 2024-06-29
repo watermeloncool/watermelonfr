d1ecaafb2b872162841bfb4a307a226fed8b4688
watermeloncool <149166592+watermeloncool@users.noreply.github.com>
2024-06-29 19:27:17 +0200

    Add files via upload

--- code/articles.js
+++ code/articles.js
@@ -4,3 +4,10 @@ document.head.insertAdjacentHTML('beforeend', `
     <meta name="robots" content="index,follow">
     <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0">
 `);
+
+
+// Ajouter <div class="article"> au début du body
+document.body.insertAdjacentHTML('afterbegin', '<div class="article">');
+
+// Ajouter </div> à la fin du body
+document.body.insertAdjacentHTML('beforeend', '</div>');
