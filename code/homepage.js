3240c6f7919cadfba6062cc8fe9ca7cc3cadb8ac
watermeloncool <149166592+watermeloncool@users.noreply.github.com>
2024-07-02 19:05:04 +0200

    Add files via upload

--- code/homepage.js
+++ code/homepage.js
@@ -60,4 +60,46 @@ const dernierArticle = articles[articles.length - 1];
 dernierArticle.style.border = 'none';
 }
 
+// technologies -----------------------------------------------------------------------
+const mots = [
 
+"ChatGPT",
+"Llama",
+"Qwen",
+"Gemma",
+
+"Python",
+"Javascript",
+"HTML",
+"CSS",
+
+"macOS",
+"Android",
+"iOS",
+"Kotlin",
+"SwiftUI",
+
+"WordPress",
+"Shopify",
+"Magento",
+"PrestaShop"
+
+];
+
+function shuffle(array) {
+  for (let i = array.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      [array[i], array[j]] = [array[j], array[i]];
+  }
+  return array;
+}
+let aleatoire = shuffle(mots);
+
+const epaisseur = ['100', '200', '300', '400', '500', '600', '700', '800', '900'];   
+const taille = ['10', '11', '12', '13', '14', '15', '16', '17', '18', '19', '20', '21', '22', '23', '24', '25', '26', '27', '28', '29', '30', '31', '32', '33', '34', '35', '36', '37', '38', '39']; 
+
+for (let i = 0; i < aleatoire.length; i++) {
+const epaisseuraleatoire = epaisseur[Math.floor(Math.random() * epaisseur.length)]; 
+const taillealeatoire = taille[Math.floor(Math.random() * taille.length)]; 
+document.getElementById('mots').innerHTML = document.getElementById('mots').innerHTML + '<div class="mot" style="font-weight:'+epaisseuraleatoire+'; font-size:'+taillealeatoire+'px; ">'+aleatoire[i]+'</div>';
+}
